5db813e005eccb52ca2087c655304fc6e7d8115f
Jake Nagel <jdnagel1@gmail.com>
2019-05-20 20:24:22 -0700

    add show and hide toggles

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
+// $(document).ready(function() {
+//   $("h1").click(function() {
+//     alert("This is a header.");
+//   });
+//
+//   $("p").click(function() {
+//     alert("These are lyrics to a verse.")
+//   });
+//
+//   $("img").click(function() {
+//     alert("This is an image of a walrus in a red sportcoat.")
+//   });
+// });
+
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
-
-  $("p").click(function() {
-    alert("These are lyrics to a verse.")
-  });
-
-  $("img").click(function() {
-    alert("This is an image of a walrus in a red sportcoat.")
+  $(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
 });
