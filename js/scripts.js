3b2acb3e6ed6ae923ee94a5bfd1d1b59e0b5b030
Jake Nagel <jdnagel1@gmail.com>
2019-05-20 19:28:16 -0700

    add 3 click event handlers using jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("These are lyrics to a verse.")
+  });
+
+  $("img").click(function() {
+    alert("This is an image of a walrus in a red sportcoat.")
+  });
 });
